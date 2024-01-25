c34a39d30244a90d3225674cc9ed96f55b3085e2
NgQuan <nguyen.phuoc.hoang.quan@gmail.com>
2024-01-25 11:44:03 +0100

    connect to database

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { render } = require('ejs');
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
-
+const connection = require('./config/database');
 const port = process.env.PORT
 // console.log(process.env)
 const app = express()
@@ -18,6 +18,16 @@ configViewEngine(app); // truyen ham express vao
 //khai bao routes
 app.use('/',webRouter); // co the them '/' phia truoc de tao them duong dan 
 
+
+//simple query
+connection.query(
+  'SELECT * FROM `users`',
+  function (err, results, fields) { //err for check error with if()
+    
+    console.log(results); // results contains rows returned by server
+    // console.log(fields); fields contains extra meta data about results, if available
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
